08f7b1566f5c05f0d85898ec89eb491700b62c24
bcaglayan <batuhan@thundra.io>
2023-01-03 09:31:23 +0300

    Tryer class added

---
+++ src/main/java/com/tryer/sonotype/publish/tryer/Dnm.java
@@ -0,0 +1,17 @@
+package com.tryer.sonotype.publish.tryer;
+
+public class Dnm {
+
+	private String field1;
+
+	public Dnm() {
+	}
+
+	public Dnm(String field1) {
+		this.field1 = field1;
+	}
+	
+	public String getField1() {
+		return field1;
+	}
+}
